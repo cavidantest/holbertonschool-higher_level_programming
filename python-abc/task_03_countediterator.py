d1d9950678fc39ae8cfc4fa3c06992ca5d968739
cavidantest <contact.cavidan.sultan@gmail.com>
2026-04-08 06:36:33 +0000

    task 2

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module that defines a CountedIterator class."""
+
+
+class CountedIterator:
+    """Iterator that counts how many items have been iterated."""
+
+    def __init__(self, iterable):
+        """Initialize with an iterable and counter."""
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __iter__(self):
+        """Return the iterator itself."""
+        return self
+
+    def __next__(self):
+        """Return next item and increment counter."""
+        item = next(self.iterator)
+        self.count += 1
+        return item
+
+    def get_count(self):
+        """Return number of iterated items."""
+        return self.count
