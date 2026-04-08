c01668fe5b90c5f21522b69645292e20f2a1f0cc
cavidantest <contact.cavidan.sultan@gmail.com>
2026-04-08 06:07:28 +0000

    task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Module that checks if an object is exactly an instance of a class."""
+"""Module that checks object class."""
 
 
 def is_same_class(obj, a_class):
-    """Return True if obj is exactly an instance of a_class, otherwise False."""
+    """Return True if obj is exactly instance of a_class."""
     return type(obj) is a_class
+    
