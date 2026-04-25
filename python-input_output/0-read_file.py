3f9080d84f5d764f9ff405586c16affca6c1d9cb
Cyborg-spec <cavidan.sultan@gmail.com>
2026-04-25 21:39:26 +0400

    some

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Contains a function that reads a text file and prints it to stdout.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a UTF8 text file and prints it to stdout.
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
