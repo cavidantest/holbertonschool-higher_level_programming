9cdd29954b6eda1f27812242e4cf8b33fdf3d8ee
Cyborg-spec <cavidan.sultan@gmail.com>
2026-04-25 21:42:48 +0400

    some

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Contains a function that returns an object represented by a JSON string.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns an object (Python data structure) represented by a JSON string.
+    """
+    return json.loads(my_str)
