63fe4c01508e02dd297d025bacfe12552baaf96b
Cyborg-spec <cavidan.sultan@gmail.com>
2026-04-25 21:40:49 +0400

    some

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Contains a function that writes a string to a text file and returns
+the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a UTF8 text file and returns the number of
+    characters written.
+    """
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return f.write(text)
