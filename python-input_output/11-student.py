44df5a5bc154c7842e09c50228a565971ca88494
Cyborg-spec <cavidan.sultan@gmail.com>
2026-04-25 21:46:28 +0400

    some

---
+++ python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Contains the class Student.
+"""
+
+
+class Student:
+    """
+    Defines a student by first_name, last_name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the student with first_name, last_name, and age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        If attrs is a list of strings, only attribute names contained
+        in this list must be retrieved.
+        """
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
