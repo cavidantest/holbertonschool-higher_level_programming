e55dcac65fc7e70823ca312eb591d5ca809c3220
Cyborg-spec <cavidan.sultan@gmail.com>
2026-04-25 21:44:55 +0400

    some

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Contains a function that returns the dictionary description for JSON
+serialization of an object.
+"""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure for
+    JSON serialization of an object.
+    """
+    return obj.__dict__
